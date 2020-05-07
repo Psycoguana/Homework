e690ed91fba9250f70b62deaaff91cb12fc0ff96
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-05-07 16:54:05 -0300

    Por un segundo creí que iba a necesitar punteros

---
+++ Cap5/punto_13.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int i, j;
+#define row 8
+#define column 8
+
+int inflateMatrix(int matrix[][column], int arrayRow, int arrayColumn);
+int printMatrix(int matrix[][column], int arrayRow, int arrayColumn);
+int randNumbGenerator(int max_rand, int seed);
+
+int main(int argc, char const *argv[]) {
+  int matriz[row][column];
+
+  int randNumb1 = randNumbGenerator(7, time(0));
+  sleep(1);  // Sorry, no sé como crear diferentes seeds sin esperar 1 sec.
+  int randNumb2 = randNumbGenerator(7, time(0));
+
+  // Darle el valor de 0 a toda la matriz
+  inflateMatrix(matriz, row, column);
+
+  // Cambiar dos posiciones aleatorias
+  matriz[randNumb1][randNumb2] = 1;
+  matriz[randNumb2][randNumb1] = 1;
+
+  printMatrix(matriz, row, column);
+
+  printf("\n\n");
+  return 0;
+}
+
+int printMatrix(int matrix[][column], int arrayRow, int arrayColumn) {
+  printf("Printeando valores de matrices...\n\n");
+  for (i = 0; i < arrayRow; i++) {
+    for (j = 0; j < arrayColumn; j++) {
+      printf("Posición de matriz: [%d][%d]\nValor: %d\n\n", i, j, matrix[i][j]);
+    }
+  }
+}
+
+int inflateMatrix(int matrix[][column], int arrayRow, int arrayColumn) {
+  for (i = 0; i < arrayRow; i++) {
+    for (j = 0; j < arrayColumn; j++) {
+      matrix[i][j] = 0;
+    }
+  }
+}
+
+int randNumbGenerator(int max_rand, int seed) {
+  srand(seed);
+  return (rand() % max_rand) + 1;
+}
