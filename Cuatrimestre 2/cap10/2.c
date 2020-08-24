461c9f6006ae3deaf839c024e67eacf69baf19f7
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-08-24 15:06:27 -0300

    Refacor

--- Cuatrimestre 2/cap10/2.c
+++ Cuatrimestre 2/cap10/2.c
@@ -1,21 +1,24 @@
-// Construir un programa que determine si “PATH” se encuentra entre las variables ce entorno.
+// Construir un programa que determine si “PATH” se encuentra entre las variables de entorno.
 //     Realizar una comparación para que no importenlas diferencias en mayúsculas y minúsculas.
+
+// El código es horrible pero funciona.
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-bool getEnv(char *[], char[]);
-bool myStrCmp(char[], char[]);
-char lower(char);
-bool isUpper(char);
+bool getEnv(char[], char[]);
+bool isUpper(char *);
+char lower(char *);
 
 int main(int argc, char const *argv[], char const *envp[]) {
   int i;
+  char wantedEnv[] = "PaTh";
 
   for (i = 0; *(envp + i); i++) {
-    if (myStrCmp(*(envp + i), "PATh")) {
+    if (getEnv(*(envp + i), wantedEnv)) {
+      printf("La variable %s se encuentra entre las variables de entorno, y su valor es:\n\n", wantedEnv);
       printf("%s", *(envp + i));
     }
   }
@@ -24,40 +27,38 @@ int main(int argc, char const *argv[], char const *envp[]) {
   return 0;
 }
 
-bool myStrCmp(char s1[], char s2[]) {
+bool getEnv(char s1[], char s2[]) {
   int i = 0;
+  char currentEnv;
 
-  char currentChar;
-  char currentChar2;
+  while (currentEnv != "=") {
+    currentEnv = *(s1 + i);
+    char wantedEnv = *(s2 + i);
 
-  do {
-    char currentChar = *(s1 + i);
-    char currentChar2 = *(s2 + i);
-
-    if (!currentChar || !currentChar2) {
+    if (!currentEnv || !wantedEnv) {
       return true;
     }
 
-    if (isUpper(currentChar)) {
-      currentChar = (lower(currentChar));
+    if (isUpper(&currentEnv)) {
+      currentEnv = (lower(&currentEnv));
     }
 
-    if (isUpper(currentChar2)) {
-      currentChar2 = (lower(currentChar2));
+    if (isUpper(&wantedEnv)) {
+      wantedEnv = (lower(&wantedEnv));
     }
 
-    if (currentChar != currentChar2) {
+    if (currentEnv != wantedEnv) {
       return false;
     }
     i++;
-  } while (currentChar != "=");
+  }
 }
 
-char lower(char ch) { return (char)(((int)ch) + 32); }
+char lower(char *ch) { return *ch + 32; }
 
-bool isUpper(char ch) {
-  if ((int)ch < 65 || (int)ch > 90) {
+bool isUpper(char *ch) {
+  // Si no es mayúscula...
+  if (*ch < 65 || *ch > 90) {
     return false;
   }
-  return true;
 }
