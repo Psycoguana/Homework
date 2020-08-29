1e3c4becd0e68ab8fcc5b96382b9a88aa50bf268
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-08-29 20:23:20 -0300

    Ahora funciona con punteros n.n

--- Cuatrimestre 2/cap14/3.c
+++ Cuatrimestre 2/cap14/3.c
@@ -11,26 +11,29 @@ struct Ventas {
   float monto;
 };
 
-int input_venta(struct Ventas, FILE *);
-int record_venta(struct Ventas, FILE *);
-int read_venta(FILE *);
+int input_venta(struct Ventas *, FILE *);
+int record_venta(struct Ventas *, FILE *);
+int read_venta(struct Ventas *, FILE *);
 
 int main(int argc, char const *argv[]) {
 
   FILE *file = fopen("3.txt", "w+b");
 
-    if (!file)
+  if (!file) {
+    printf("Algo salió horriblemente mal abriendo el archivo D:\n\n");
+    exit(EXIT_FAILURE);
+  }
 
   struct Ventas ventas;
-  input_venta(ventas, file);
-  read_venta(file);
+  input_venta(&ventas, file);
+  read_venta(&ventas, file);
 
   fclose(file);
   printf("\n\n");
   return 0;
 }
 
-int input_venta(struct Ventas ventas, FILE *file) {
+int input_venta(struct Ventas *ventas, FILE *file) {
   int size, i;
   float monto_vendido;
 
@@ -38,47 +41,45 @@ int input_venta(struct Ventas ventas, FILE *file) {
   scanf("%d", &size);
 
   for (i = 0; i < size; i++) {
-    printf("Nombre del vendedor: ");
+    printf("\nNombre del vendedor: ");
     clean_stdin();
-    gets(ventas.vendedor);
+    gets(ventas->vendedor);
 
-    printf("Ingrese el monto vendio: ");
+    printf("Ingrese el monto vendido: ");
     scanf("%f", &monto_vendido);
-    ventas.monto = monto_vendido;
+    ventas->monto = monto_vendido;
 
     record_venta(ventas, file);
-    printf("\n");
   }
 }
 
-int record_venta(struct Ventas ventas, FILE *file) { fwrite(&ventas, sizeof(ventas), 1, file); }
+int record_venta(struct Ventas *ventas, FILE *file) { fwrite(ventas, sizeof(*(ventas)), 1, file); }
 
-int read_venta(FILE *file) {
+int read_venta(struct Ventas *ventas, FILE *file) {
 
   fseek(file, 0, SEEK_SET);
-  struct Ventas ventas;
   float max_venta = 0;
   int i = 0, best_seller_index;
 
-  fread(&ventas, sizeof(ventas), 1, file);
+  fread(ventas, sizeof(*(ventas)), 1, file);
 
   while (!feof(file)) {
     // Si el monto actual es el más grande que se haya encontrado...
-    if (ventas.monto > max_venta) {
+    if (ventas->monto > max_venta) {
       // Actualiza el monto más grande y el index del struct.
-      max_venta = ventas.monto;
+      max_venta = ventas->monto;
       best_seller_index = i;
     }
-    fread(&ventas, sizeof(ventas), 1, file);
+    fread(ventas, sizeof(*(ventas)), 1, file);
     i++;
   }
 
   // Me desplazo hasta el mejor vendedor...
-  fseek(file, best_seller_index * (sizeof(ventas)), SEEK_SET);
+  fseek(file, best_seller_index * (sizeof(*(ventas))), SEEK_SET);
   // Cargo el contenido del struct en disco a la memoria temporal...
-  fread(&ventas, sizeof(ventas), 1, file);
+  fread(ventas, sizeof(*(ventas)), 1, file);
 
-  printf("\nEl mejor vendedor es %s, con un monto de ventas de %.2f", ventas.vendedor, ventas.monto);
+  printf("\nEl mejor vendedor es %s, con un monto de ventas de $%.2f", ventas->vendedor, ventas->monto);
 }
 
 void clean_stdin(void) {
