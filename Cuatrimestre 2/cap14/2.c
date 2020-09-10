6464a06aabdccd7bf7f668db65e8c27b38068b9e
Psycoguana <21992381+Psycoguana@users.noreply.github.com>
2020-09-10 19:53:08 -0300

    Funciona con punteros, no entiendo los sizes :'(

--- Cuatrimestre 2/cap14/2.c
+++ Cuatrimestre 2/cap14/2.c
@@ -7,15 +7,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-int find_sequence(FILE *, char [], int);
+int find_sequence(FILE *, char *, int);
 
 int main(int argc, char const *argv[]) {
-
-  unsigned char sequence_start = 'A';
-  unsigned char sequence_end = 'F';
   unsigned char sequence[] = "A85C697070AFEF";
-  unsigned char possible_sequence[sizeof(sequence)];
-  
 
   FILE *file = fopen("1.txt", "rb");
   if (!file) {
@@ -29,29 +24,30 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
-int find_sequence(FILE *file, char sequence[], int sequence_size) {
+int find_sequence(FILE *file, char *sequence, int sequence_size) {
   unsigned char current_char;
-  unsigned char sequence_start = sequence[0];
-  unsigned char sequence_end = 'F';
   unsigned char possible_sequence[sequence_size];
 
+  unsigned char sequence_start = *(sequence);
+  // Por alguna razón, sequence_size == 15, a pesar de solo tiene 14 caractéres incluyendo a '\0'.
+  unsigned char sequence_end = *(sequence + sequence_size - 2);
 
   fread(&current_char, sizeof(unsigned char), 1, file);
   while (!feof(file)) {
-    int current_position = ftell(file);
+
     if (current_char == sequence_start) {
-      fseek(file, 0, sequence_size);
+      fseek(file, sequence_size-3, SEEK_CUR);
       fread(&current_char, sizeof(unsigned char), 1, file);
       if (current_char == sequence_end) {
-        // Esto falla porque tengo que usar el arg offset para
-        // especificar posiciones != principio/final del archivo.
-        // int a = fseek(file, 0, current_position);
 
-        fseek(file, -sequence_size + 3, SEEK_CUR);
+        fseek(file, -sequence_size+1, SEEK_CUR);
         fread(&possible_sequence, sequence_size, 1, file);
+        
         if (strcmp(sequence, possible_sequence) == 0) {
-          printf("Se encontró la secuencia %s luego de leer %li bytes :)", possible_sequence, ftell(file));
-          fseek(file, 0, SEEK_END); // Termina el programa.
+          printf("Se encontró la secuencia %s luego de leer %li bytes :)\n\n", possible_sequence, ftell(file));
+          // Cierro el archivo y termino el programa.
+          fclose(file);
+          exit(EXIT_SUCCESS);
         }
       }
     } else {
